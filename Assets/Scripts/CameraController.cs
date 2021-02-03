c75e314061a4175bd0df98f6b83588ec8e9d9b1d
frederik desmet <info@neo-level.com>
2021-02-03 10:43:34 +0100

    Addition of camera functionality.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 _offset;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _offset = transform.position - player.transform.position;
+        
+    }
+
+    // Best for camera code.
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + _offset;
+    }
+    
+}
