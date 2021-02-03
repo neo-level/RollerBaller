cdb820ac2d24190b8092454bbfdc4e669621a72e
frederik desmet <info@neo-level.com>
2021-02-03 10:59:00 +0100

    handled static rotation for the pickups

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private Vector3 _rotation = new Vector3(15, 30, 45);
+
+    private void Update()
+    {
+        transform.Rotate(_rotation * Time.deltaTime);
+    }
+}
