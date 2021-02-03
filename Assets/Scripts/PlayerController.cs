cfc64c4fef6d0b40862fc65917bc765b8446ce36
frederik desmet <info@neo-level.com>
2021-02-03 10:09:33 +0100

    Addition of Movement to the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    
+    private float _movementX;
+    private float _movementY;
+    
+    private Rigidbody _rigidbody;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    // Best place for physics related code.
+    private void FixedUpdate()
     {
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
         
+        _rigidbody.AddForce(movement * speed);
+    }
+    
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
+
     }
 }
