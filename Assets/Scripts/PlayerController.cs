a7e821b450a1940823e2c73d3f56f43b1e84b2fc
frederik desmet <info@neo-level.com>
2021-02-03 12:04:57 +0100

    Created a basic ui displaying the collected pickups and a win game when all are collected.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private short _count;
+    private short _totalPickups = 12;
+
     private float _movementX;
     private float _movementY;
-    
+
     private Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+
+        _count = 0;
         
+        winTextObject.SetActive(false);
+        
+        SetCountText();
     }
 
 
@@ -24,17 +36,27 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
-        
+
         _rigidbody.AddForce(movement * speed);
     }
-    
+
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
         _movementX = movementVector.x;
         _movementY = movementVector.y;
+    }
 
+    private void SetCountText()
+    {
+        countText.text = $"Count {_count}";
+
+        if (_count >= _totalPickups)
+        {
+            winTextObject.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -42,7 +64,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            _count += 1;
+            SetCountText();
         }
-        
     }
 }
